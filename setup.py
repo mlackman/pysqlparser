da78bde1f9e656c0bd2e28b91b5bd9c48229fd9a
Mika Lackman <mika.lackman@gmail.com>
2016-12-13 08:39:12 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(name='PySQLParser',
+      version='0.01',
+      description='Python SQL parser',
+      author='Mika Lackman',
+      author_email='mika.lackman@gmail.com',
+      packages=['pysqlparser'])
+
