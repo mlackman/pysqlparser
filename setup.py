b399bfef67149e8a283473cb55f50fabc1c02393
Mika Lackman <mika.lackman@gmail.com>
2016-12-21 13:53:01 +0200

    impl

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(name='PySQLParser',
-      version='0.01',
-      description='Python SQL parser',
-      author='Mika Lackman',
-      author_email='mika.lackman@gmail.com',
-      packages=['pysqlparser'])
+  version='0.1.3',
+  description='Python SQL parser',
+  author='Mika Lackman',
+  author_email='mika.lackman@gmail.com',
+  license='LGPL',
+  packages=['pysqlparser', 'pysqlparser.ply'])
 
